6c4a3acf5ca14c42c1ad25c797d338734848349d
Tigger <warit2005@gmail.com>
2023-12-01 20:49:02 +0800

    Update on isFinalScene and ending pic.

--- adamcmith.py
+++ adamcmith.py
@@ -187,18 +187,18 @@ def AC(name):
             #Day 7 End
             #________________________________________________________________________________Sound__________________________________________________________________________________________________
             #Good ending index link to 114
-            ("[You and Adam go to prom to gether]", "pictures/dog.png", [115]),
-            ("[Congratulations You Win]", "pictures/dog.png", [115]),
+            ("[You and Adam go to prom to gether]", "pictures/Prom Night.png", [115]),
+            ("[Congratulations You Win]", "pictures/Prom Night.png", [], True),
             #Bad ending index link 116
-            ("Adam felt distence from you", "pictures/dog.png", [117]),
-            ("You Lose", "pictures/dog.png", [117]),
+            ("Adam felt distence from you", "pictures/No Prom.png", [117]),
+            ("You Lose", "pictures/No prom.png", [117], True),
             #Adjustment to Options Update
             ("Thought: That's Adam! I haven't seen him in days. He looks so stressed... I wonder what he's studying.", "pictures/AdamCmith_Pic/003.png", [2], name),#118
             ("Thought: Wow, Adam's here too? That's unexpected. It's nice to see a familiar face, though.", "pictures/AdamCmith_Pic/103.png", [19], name),#119
             ("Thought: That's Adam... I didn't know he had an interest in photography. He seems so peaceful here." , "pictures/AdamCmith_Pic/204.png", [35], name),#120
             ("Thought: Volunteering at a dog shelter with Adam... It's really been a while since we went together." , "pictures/AdamCmith_Pic/304.png", [51], name),#121
             ("Thought: I had no idea Adam was into photography to this extent. His pictures are really impressive." , "pictures/AdamCmith_Pic/403.png", [67], name),#122
-            ("What do you expect? You just friend zoned him...." , "pictures/AdamCmith_Pic/513.png", [123], "GM", "sounds/fzending.wav"),#123
+            ("What do you expect? You just friend zoned him...." , "pictures/AdamCmith_Pic/513.png", [123], "GM", "sounds/fzending.wav", True),#123
             ("[As they walk side by side on the sandy shore, the fading sunlight casts a warm glow, creating a serene and intimate atmosphere.]" , "pictures/AdamCmith_Pic/503.png", [82]),#124
             ("Look what I got here, he says, pulling out his camera. I took some pictures during our beach walk yesterday." , "pictures/AdamCmith_Pic/012.png", [98], "Adam Cmith", "sounds/WeirdAdam.wav")#125
 
